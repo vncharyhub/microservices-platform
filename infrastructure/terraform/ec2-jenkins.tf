6c50249a66eea65009a9f600c6e35b1141a8ca7c
DevOps_Culture_in_SDLC <90929082+vncharyhub@users.noreply.github.com>
2025-06-04 21:34:41 +0530

    Update ec2-jenkins.tf

--- infrastructure/terraform/ec2-jenkins.tf
+++ infrastructure/terraform/ec2-jenkins.tf
@@ -1,1 +1,10 @@
-
+# ec2-jenkins.tf
+resource "aws_instance" "jenkins" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t3.medium"
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  key_name      = "jenkins-key"
+  tags = {
+    Name = "JenkinsController"
+  }
+}
